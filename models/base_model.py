619b62a7a034d41c2009b6f4577a6b0d6082bd86
hxc <cei196505153@163.com>
2024-10-16 12:54:25 +0800

    update base_model

--- models/base_model.py
+++ models/base_model.py
@@ -39,3 +39,82 @@ class BaseModel(nn.Module):
     def optimize_parameters(self):
         pass
 
+    def setup(self, conf):
+        if self.isTrain:
+            self.schedulers = []
+        if not self.isTrain or conf.continue_train:
+            if conf.load_iter == 'latest':
+                load_suffix = 'latest'
+            else:
+                load_suffix = 'iter_%d' % conf.load_iter if conf.load_iter > 0 else conf.epoch
+            self.load_networks(load_suffix)
+        self.print_networks(conf.verbose)
+
+    def eval(self):
+        for name in self.model_names:
+            if isinstance(name, str):
+                net = getattr(self, 'net' + name)
+                net.eval()
+
+    def test(self):
+        with torch.no_grad():
+            self.forward()
+            self.compute_visuals()
+
+    def compute_visuals(self):
+        pass
+
+    def update_learning_rate(self):
+        old_lr = self.optimizers[0].param_groups[0]['lr']
+        for scheduler in self.schedulers:
+            if self.conf.lr_policy == 'plateau':
+                scheduler.step(self.metric)
+            else:
+                scheduler.step()
+        lr = self.optimizers[0].param_groups[0]['lr']
+        print('learning rate %.7f -> %.7f' % (old_lr, lr))
+
+    def get_current_visuals(self):
+        visual_ret = OrderedDict()
+        for name in self.visual_names:
+            if isinstance(name, str):
+                visual_ret[name] = getattr(self, name)
+        return visual_ret
+
+    def get_current_losses(self):
+        errors_ret = OrderedDict()
+
+        for name in self.loss_names:
+            if isinstance(name, str):
+                errors_ret[name] = float(getattr(self, 'loss_' + name))
+        return errors_ret
+
+    def save_networks(self, epoch):
+        if epoch =='latest' and self.emaG:
+            self.emaG.apply_shadow()
+            print('The latest using EMA.')
+        for name in self.model_names:
+            if isinstance(name, str):
+                save_filename = '%s_net_%s.pth' % (epoch, name)
+                save_path = os.path.join(self.save_dir, save_filename)
+                net = getattr(self, 'net'+name)
+
+                # if len(self.gpu_ids)>0 and torch.cuda.is_available():
+                #     torch.save(net.module.cpu().state_dict(), save_path)
+                #     net.cuda(self.gpu_ids[0])
+                # else:
+                torch.save(net.state_dict(), save_path)
+
+    def __patch_instance_norm_state_dict(self, state_dict, module, keys, i=0):
+        """Fix InstanceNorm checkpoints incompatibility (prior to 0.4)"""
+        key = keys[i]
+        if i + 1 == len(keys):  # at the end, pointing to a parameter/buffer
+            if module.__class__.__name__.startswith('InstanceNorm') and \
+                    (key == 'running_mean' or key == 'running_var'):
+                if getattr(module, key) is None:
+                    state_dict.pop('.'.join(keys))
+            if module.__class__.__name__.startswith('InstanceNorm') and \
+               (key == 'num_batches_tracked'):
+                state_dict.pop('.'.join(keys))
+        else:
+            self.__patch_instance_norm_state_dict(state_dict, getattr(module, key), keys, i + 1)
